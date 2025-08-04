d262409255610572482e53c653ef5c76de36d62d
RamitVishwakarma <vishwakarmaramit@gmail.com>
2025-08-04 15:14:44 +0530

    Fixed video links

--- src/components/SectionTwo/data.ts
+++ src/components/SectionTwo/data.ts
@@ -8,33 +8,33 @@ export const dataOne: dataInterface = {
   description:
     "Industrial AI helped a multi-billion dollar manufacturer automate their quoting process, reducing time-to-quote and boosting sales volume. ",
   descButtonText: "Explore Quoting Automation ",
-  videoUrl: "https://www.getindustrial.ai/records/quoting.mp4",
+  videoUrl: "/videos/assembly.mp4",
 };
 
 export const dataTwo: dataInterface = {
   description:
     "Industrial AI worked with a leading metals enterprise to supercharge their materials requirements planning process with AI, reducing scrap rates.",
   descButtonText: "Explore Material Planning Automation ",
-  videoUrl: "https://www.getindustrial.ai/records/materials-planning.mp4",
+  videoUrl: "/videos/warehouseInv.mp4",
 };
 
 export const dataThree: dataInterface = {
   description:
     "Industrial AI worked with a leading metals enterprise to integrate AI into their  production scheduling process, reducing unplanned downtime.",
   descButtonText: "Explore Scheduling Automation",
-  videoUrl: "https://www.getindustrial.ai/records/scheduling.mp4",
+  videoUrl: "/videos/schedulingAutomation.mp4",
 };
 
 export const dataFour: dataInterface = {
   description:
     "Industrial AI helped a leading structural engineering group harness Generative Analytics for their manual and expensive environmental reporting process.",
   descButtonText: "Explore Reporting Automation ",
-  videoUrl: "https://www.getindustrial.ai/records/environmental.mp4",
+  videoUrl: "/videos/reportingAutomation.mp4",
 };
 
 export const dataFive: dataInterface = {
   description:
     "Industrial AI worked with a rapidly-growing wholesaler to organize their structured and unstructured data for pricing, reducing errors and improving efficiency.",
   descButtonText: "Explore Pricing Automation",
-  videoUrl: "https://www.getindustrial.ai/records/pricing.mp4",
+  videoUrl: "/videos/pricingAutomation.mp4",
 };
